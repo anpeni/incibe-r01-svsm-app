6f9014ffa66046e30bd5996b30dfa5bdd4cf4932
AntonioPerez <p-aperez@neoris.com>
2023-07-17 09:22:34 +0200

    fix(backstage): tekton changes

--- packages/backend/src/plugins/tekton-pipelines.ts
+++ packages/backend/src/plugins/tekton-pipelines.ts
@@ -1,18 +1,18 @@
-// import { createRouter } from '@jquad-group/plugin-tekton-pipelines-backend';
-// import { Router } from 'express';
-// import { PluginEnvironment } from '../types';
+import { createRouter } from '@jquad-group/plugin-tekton-pipelines-backend';
+import { Router } from 'express';
+import { PluginEnvironment } from '../types';
 
-// export default async function createPlugin(
-//   env: PluginEnvironment,
-// ): Promise<Router> {
+export default async function createPlugin(
+  env: PluginEnvironment,
+): Promise<Router> {
 
-//   return await createRouter({
-//     logger: env.logger,
-//     config: env.config,
+  return await createRouter({
+    logger: env.logger,
+    config: env.config,
     
     
-//   });
-// }
+  });
+}
 
 // function getBearerTokenFromAuthorizationHeader(authorizationHeader: unknown): string | undefined {
 //   if (typeof authorizationHeader === 'string') {
@@ -26,56 +26,56 @@
 //   return undefined;
 // }
 
-import { createRouter } from '@jquad-group/plugin-tekton-pipelines-backend';
-import { Router } from 'express';
-import { PluginEnvironment } from '../types';
-import { Request } from 'express';
+// import { createRouter } from '@jquad-group/plugin-tekton-pipelines-backend';
+// import { Router } from 'express';
+// import { PluginEnvironment } from '../types';
+// import { Request } from 'express';
 
-interface RequestWithToken extends Request {
-  token?: string;
-}
+// interface RequestWithToken extends Request {
+//   token?: string;
+// }
 
-// Esta es la función que extrae el token del encabezado de autorización
-function getBearerTokenFromAuthorizationHeader(authorizationHeader: unknown): string | undefined {
-  if (typeof authorizationHeader !== 'string') {
-    return undefined;
-  }
+// // Esta es la función que extrae el token del encabezado de autorización
+// function getBearerTokenFromAuthorizationHeader(authorizationHeader: unknown): string | undefined {
+//   if (typeof authorizationHeader !== 'string') {
+//     return undefined;
+//   }
 
-  const parts = authorizationHeader.split(' ');
+//   const parts = authorizationHeader.split(' ');
 
-  if (parts.length !== 2 || parts[0] !== 'Bearer') {
-    return undefined;
-  }
+//   if (parts.length !== 2 || parts[0] !== 'Bearer') {
+//     return undefined;
+//   }
 
-  return parts[1];
-}
+//   return parts[1];
+// }
 
-export default async function createPlugin(
-  env: PluginEnvironment,
-): Promise<Router> {
-  const router = await createRouter({
-    logger: env.logger,
-    config: env.config,
-  });
+// export default async function createPlugin(
+//   env: PluginEnvironment,
+// ): Promise<Router> {
+//   const router = await createRouter({
+//     logger: env.logger,
+//     config: env.config,
+//   });
 
-  // Middleware para extraer el token Bearer de la cabecera de autorización
-  router.use((req: RequestWithToken, res, next) => {
-    const authorizationHeader = req.headers['authorization'];
-    const token = getBearerTokenFromAuthorizationHeader(authorizationHeader);
+//   // Middleware para extraer el token Bearer de la cabecera de autorización
+//   router.use((req: RequestWithToken, res, next) => {
+//     const authorizationHeader = req.headers['authorization'];
+//     const token = getBearerTokenFromAuthorizationHeader(authorizationHeader);
 
-    if (!token) {
-      res.status(401).send('Unauthorized: No token provided');
-      return;
-    }
+//     if (!token) {
+//       res.status(401).send('Unauthorized: No token provided');
+//       return;
+//     }
 
-    // Aquí puedes hacer algo con el token, por ejemplo, adjuntarlo al objeto de la solicitud.
-    req.token = token;
+//     // Aquí puedes hacer algo con el token, por ejemplo, adjuntarlo al objeto de la solicitud.
+//     req.token = token;
     
-    next();  // Continúa con la siguiente función de middleware o la función de manejo de la ruta
-  });
+//     next();  // Continúa con la siguiente función de middleware o la función de manejo de la ruta
+//   });
 
-  // Tus rutas aquí
+//   // Tus rutas aquí
 
-  return router;
-}
+//   return router;
+// }
 
