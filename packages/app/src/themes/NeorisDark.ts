5dcbd50aae62e6410998e7ad4ddf10505f6703ad
Angel David Sanchez Lopez <angel.sanchezl@neoris.com>
2023-10-25 16:17:55 +0200

    fix: Adjusted Button Colors for Improved Dark Mode Compatibility

--- packages/app/src/themes/NeorisDark.ts
+++ packages/app/src/themes/NeorisDark.ts
@@ -15,7 +15,6 @@ import { AutocompleteClassKey } from '@material-ui/lab/Autocomplete';
 import { AlertClassKey } from '@material-ui/lab/Alert';
 import { vars } from './variables';
 
-
 // Labs types not included in overrides; https://github.com/mui/material-ui/issues/19427
 declare module '@material-ui/core/styles/overrides' {
   export interface ComponentNameToClassKey {
@@ -23,7 +22,6 @@ declare module '@material-ui/core/styles/overrides' {
     MuiAutocomplete: AutocompleteClassKey;
   }
 }
-
 
 const pageThemesFontColorOverride: Record<string, PageTheme> = {};
 Object.keys(defaultPageThemes).map(key => {
@@ -264,6 +262,7 @@ const createCustomThemeOverrides = (
       // ? Boton Add Catalog
       containedPrimary: {
         backgroundColor: vars.dark.background.highlight,
+        color: vars.dark.fontColor.white,
       },
       // ? Ajustar Control panel en Sidebar
       label: {
